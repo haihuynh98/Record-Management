0aa722922163c4eda934dc33ff9caed0415cc905
Hari Huynh <haihuynh@tepaylink.com.vn>
2025-08-29 17:02:24 +0700

    add thêm 3s

--- app/Filament/Resources/ProfileResource.php
+++ app/Filament/Resources/ProfileResource.php
@@ -150,7 +150,7 @@ class ProfileResource extends Resource implements HasShieldPermissions
             ->defaultSort('created_at', 'desc')
             ->paginated([25, 50, 100])
             ->defaultPaginationPageOption(50)
-            ->poll('2s')
+            ->poll('3s')
             ->recordUrl(null)
             ->actions([
                 \Filament\Tables\Actions\Action::make('view')
